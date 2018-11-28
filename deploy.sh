0a7e3506085dd7c51febdf9ca30624a380e012ce
chpmnrssll <chpmnrssll@gmail.com>
2018-11-27 20:29:26 -0700

    Add .travis.yml config and update deploy script to deploy from travis

--- deploy.sh
+++ deploy.sh
@@ -7,20 +7,20 @@ set -e
 npm run build
 
 # navigate into the build output directory
-cd .vuepress/dist
+# cd .vuepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
 # Create .nojekyll to prevent Github Pages build errors
-touch .nojekyll
+touch .vuepress/dist/.nojekyll
 
-git init
-git add -A
-git commit -m 'deploy'
+# git init
+# git add -A
+# git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:chpmnrssll/aluminium-spider.git master:gh-pages
+# git push -f git@github.com:chpmnrssll/aluminium-spider.git master:gh-pages
